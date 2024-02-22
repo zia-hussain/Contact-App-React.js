f255355ec39c64b18d7cdac723125696f6b4e604
zia-hussain <syedziashahgillani76@gmail.com>
2024-02-22 16:26:16 +0500

    Saved in LocalStorage ! ...

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,20 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import "./App.css";
 import ContactCard from "./ContactCard";
 import ContactList from "./ContactList";
+
 function App() {
-  const [contacts, setContacts] = useState([]);
-  const contactappHandler = (contact) => {
-    console.log(contact)
-    setContacts([...contacts,contact])
+  let LOCAL_STORAGE_KEY = "contacts";
+
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
+  );
+  const addcontactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuidv4(), ...contact }]);
   };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div className="ui container">
       <Header />
-      <AddContact contactappHandler={contactappHandler} />
-      <ContactList contacts={contacts} />
+      <AddContact addcontactHandler={addcontactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 }
