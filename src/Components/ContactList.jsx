a3d214b5f09c0251402f6f298d9d0baed9746679
zia-hussain <syedziashahgillani76@gmail.com>
2024-02-22 16:21:23 +0500

    Delete functionality added !

--- src/Components/ContactList.jsx
+++ src/Components/ContactList.jsx
@@ -1,11 +1,20 @@
 /* eslint-disable react/prop-types */
 import ContactCard from "./ContactCard";
 
-function ContactList({ contacts }) {
+function ContactList(props) {
   // console.log(props.contacts);
 
-  const renderContactList = contacts?.map((contact) => {
-    return <ContactCard contact={contact} key={contact} />;
+  const deleteContactHandler = (id) => {
+    props.getContactId(id)
+  };
+  const renderContactList = props.contacts?.map((contact, uid) => {
+    return (
+      <ContactCard
+        contact={contact}
+        key={uid}
+        clickHandler={deleteContactHandler}
+      />
+    );
   });
 
   return <div className="contactList ui celled list">{renderContactList}</div>;
