ca3900df178b5a6de8543fd49ba2f090d2e12fe9
zia-hussain <syedziashahgillani76@gmail.com>
2024-02-23 19:25:51 +0500

    Some previous styling done better !

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="ui fixed menu">
+    <div className="header ui fixed menu">
       <div className="ui container">
         <h2>Contact Manager</h2>
       </div>
