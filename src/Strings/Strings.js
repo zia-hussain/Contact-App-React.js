e96fb94e7266f48eef92e00030275fbddb36925e
zia-hussain <syedziashahgillani76@gmail.com>
2024-02-28 14:23:56 +0500

    custon db !

---
+++ src/Strings/Strings.js
@@ -0,0 +1,12 @@
+export const contactsList = [
+  {
+    id: "312573d6-b28a-45b5-b147-6650970d28d2",
+    name: "SyedAxha Zia Shah",
+    email: "syedziashahgillani76@gmail.com",
+  },
+  {
+    id: "312573d6-b28a-45b5-b147-6650970d28d2",
+    name: "Syed Zia Shah",
+    email: "syedziashahgillani76@gmail.com",
+  },
+];
